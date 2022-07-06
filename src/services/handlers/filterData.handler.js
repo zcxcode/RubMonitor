6a3ac5ac031cddbec76233016d65977376b42fc4
zcxcode <crnxyo@gmail.com>
2022-07-07 01:39:54 +0400

    fix

--- src/services/handlers/filterData.handler.js
+++ src/services/handlers/filterData.handler.js
@@ -1,6 +1,6 @@
 export function filterHandler(data, input) {
   let result = data.filter((i) => {
-    return i.CharC1ode.toLowerCase().indexOf(input.toLowerCase()) > -1;
+    return i.CharCode.toLowerCase().indexOf(input.toLowerCase()) > -1;
   });
   return result;
 }
